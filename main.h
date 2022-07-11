af2b2260972679eb968c9426aafec7420e751b41
ngounou-tp <patriciatchuinteu@gmail.com>
2022-07-11 06:43:04 -0700

    hear file

--- main.h
+++ main.h
@@ -36,7 +36,7 @@ void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
-/* print_bases */
+/* print_different_bases */
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
